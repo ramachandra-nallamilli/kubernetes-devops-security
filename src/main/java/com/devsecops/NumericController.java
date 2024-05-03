619705da7ed9fa54c94668e305fb18cfd7aa9fbe
ramachandra-nallamilli <ramachandra.nallamilli80@gmail.com>
2024-05-03 17:57:53 +0530

    removed rpc call

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -18,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
+	//private static final String baseURL = "http://node-service:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -41,14 +41,14 @@ public class NumericController {
 			return message;
 		}
 
-		@GetMapping("/increment/{value}")
+	/* 	@GetMapping("/increment/{value}")
 		public int increment(@PathVariable int value) {
 			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
 			String response = responseEntity.getBody();
 			logger.info("Value Received in Request - " + value);
 			logger.info("Node Service Response - " + response);
 			return Integer.parseInt(response);
-		}
+		}*/
 	}
 
 }
